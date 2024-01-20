dc66250d6bc941cbbca3e5a4e355d6a8b6a73a23
Elton Lobo <eltonsw00@gmail.com>
2024-01-20 11:58:29 +0530

    Add 00645-medium-diff solution

--- playground/medium/00645-medium-diff.ts
+++ playground/medium/00645-medium-diff.ts
@@ -12,7 +12,16 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Diff<O, O1> = any
+type Diff<O, O1> = {
+  [
+  K in keyof O | keyof O1 as
+  K extends keyof O
+    ? K extends keyof O1
+      ? never
+      : K
+    : K
+  ]: K extends keyof O ? O[K] : K extends keyof O1 ? O1[K] : never
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
