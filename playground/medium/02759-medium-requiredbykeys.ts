d17734e77fc07f35c99eb759e3af5a8f54e92604
Elton Lobo <eltonsw00@gmail.com>
2024-01-26 11:07:00 +0530

    Add 02759-medium-requiredbykeys solution

--- playground/medium/02759-medium-requiredbykeys.ts
+++ playground/medium/02759-medium-requiredbykeys.ts
@@ -27,7 +27,8 @@
 
 /* _____________ Your Code Here _____________ */
 
-type RequiredByKeys<T, K> = any
+type RequiredByKeys<T, K extends keyof T = keyof T> =
+  Omit<Required<Pick<T, K>> & Omit<T, K>, never>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
