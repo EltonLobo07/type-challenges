a864000bd65f2ff6221562c8784ea8469e890149
Elton Lobo <eltonsw00@gmail.com>
2024-03-16 08:51:23 +0530

    Add 09989 solution

--- playground/medium/09989-medium-count-element-number-to-object.ts
+++ playground/medium/09989-medium-count-element-number-to-object.ts
@@ -33,7 +33,70 @@
 
 /* _____________ Your Code Here _____________ */
 
-type CountElementNumberToObject<T> = any
+type ToString<T extends number> = `${T}`
+
+type ToNum<T extends string> = T extends `${infer Num extends number}` ? Num : never
+
+type Reverse<T extends string> =
+  T extends `${infer First}${infer Rest}`
+    ? `${Reverse<Rest>}${First}`
+    : T
+
+type AddOneMp = {
+  '0': '1'
+  '1': '2'
+  '2': '3'
+  '3': '4'
+  '4': '5'
+  '5': '6'
+  '6': '7'
+  '7': '8'
+  '8': '9'
+  '9': '0'
+}
+
+type _AddOne<
+  T extends string,
+  Carry extends string = '0',
+> =
+  T extends `${infer First extends keyof AddOneMp}${infer Rest}`
+    ? `${AddOneMp[First]}${First extends '9' ? _AddOne<Rest, '1'> : Rest}`
+    : Carry extends '0'
+      ? ''
+      : '1'
+
+// Assume `T` is always a positive number
+type AddOne<T extends number> = ToNum<Reverse<_AddOne<Reverse<ToString<T>>>>>
+
+type Merge<
+  T1,
+  T2,
+> = {
+  [K in (keyof T1 | keyof T2)]: K extends keyof T2 ? T2[K] : K extends keyof T1 ? T1[K] : never
+}
+
+type Flatten<T extends ReadonlyArray<any>> =
+  T extends readonly [infer First, ...infer Rest]
+    ? IsNever<First> extends true
+      ? [First, ...Flatten<Rest>]
+      : [...(First extends ReadonlyArray<any> ? Flatten<First> : [First]), ...Flatten<Rest>]
+    : T
+
+type IsNever<T> = [T] extends [never] ? true : false
+
+type _CountElementNumberToObject<
+  T extends ReadonlyArray<any>,
+  Obj extends Record<PropertyKey, number> = { },
+> =
+  T extends readonly [infer First, ...infer Rest extends Array<any>]
+    ? IsNever<First> extends true
+      ? { }
+      : First extends PropertyKey
+        ? _CountElementNumberToObject<Rest, Merge<Obj, Record<First, First extends keyof Obj ? AddOne<Obj[First]> : 1>>>
+        : { }
+    : Obj
+
+type CountElementNumberToObject<T extends ReadonlyArray<any>> = _CountElementNumberToObject<Flatten<T>>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
