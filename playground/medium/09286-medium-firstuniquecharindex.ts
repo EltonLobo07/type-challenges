fff59965a1c1280212db5efa04a20c40a8d5a3c2
Elton Lobo <eltonsw00@gmail.com>
2024-03-16 07:40:24 +0530

    Add 09286-medium-firstuniquecharindex solution

--- playground/medium/09286-medium-firstuniquecharindex.ts
+++ playground/medium/09286-medium-firstuniquecharindex.ts
@@ -12,7 +12,44 @@
 
 /* _____________ Your Code Here _____________ */
 
-type FirstUniqueCharIndex<T extends string> = any
+type IsPresent<
+  Ch extends string,
+  Str extends string,
+> =
+  Str extends `${infer First}${infer Rest}`
+    ? First extends Ch
+      ? true
+      : IsPresent<Ch, Rest>
+    : false
+
+type Or<
+  Op1 extends boolean,
+  Op2 extends boolean,
+> =
+  Op1 extends true
+    ? true
+    : Op2
+
+type Extends<
+  Sub,
+  Super,
+> =
+  Sub extends Super
+    ? true
+    : false
+
+type _FirstUniqueCharIndex<
+  T extends string,
+  Seen extends string = never,
+  Idx extends ReadonlyArray<1> = [],
+> =
+  T extends `${infer First}${infer Rest}`
+    ? Or<Extends<First, Seen>, IsPresent<First, Rest>> extends true
+      ? _FirstUniqueCharIndex<Rest, Seen | First, [...Idx, 1]>
+      : Idx['length']
+    : -1
+
+type FirstUniqueCharIndex<T extends string> = _FirstUniqueCharIndex<T>
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
